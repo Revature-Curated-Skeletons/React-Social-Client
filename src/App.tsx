4d7263d7c2da103a28d71a5caa7384cecf09cf4f
Wilson Neely <Naruisk.art@gmail.com>
2022-01-03 11:50:12 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,30 @@ import { useAppSelector } from './app/hooks';
 const App = () => {
 
   //const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-const [theme, setTheme] = useState('dark');
+const [theme, setTheme] = useState('light');
 
 
 function switchTheme () {
-  console.log("SwitchTheme Invoked")
-  console.log(theme);
   let newTheme;
   if (theme == 'light'){
     newTheme = 'dark';
+    document.documentElement.setAttribute("data-theme", newTheme)
   }
   else{
     newTheme = 'light';
+    document.documentElement.setAttribute("data-theme", newTheme)
   }
-  console.log(theme);
   setTheme(newTheme);
-  console.log(theme);
-  //<button onClick={() => switchTheme()}>ThemeSwitcher</button>
 }
 
   const loggedIn = useAppSelector(state => state.auth[0].token);
   return (
-    <div className="App" data-theme={theme}>
+    <div className="App">
       <Stack direction="horizontal" gap={5}>
         <Navbar loggedIn={loggedIn}/>
         <div className="justify-content-center" style={{width:"100%", height:"100vh", overflowY:"scroll"}} >
           <MainRouter loggedIn={loggedIn}/>
-         
+          <button onClick={() => switchTheme()}>ThemeSwitcher</button>
         </div>
       </Stack>
     </div>
